223608ddaffbebe70d37c00b4982bdd6508dbe53
Shimon Magal <shimon.magal@gmail.com>
2021-01-01 21:37:14 +0200

    add biases

--- src/main/java/com/jtorch/scratch/DanseLayer.java
+++ src/main/java/com/jtorch/scratch/DanseLayer.java
@@ -2,8 +2,10 @@ package com.jtorch.scratch;
 
 public class DanseLayer {
     private final Matrix weights;
+    private final Matrix biases;
 
     public DanseLayer(int inputSize, int neuronSize) {
         weights = new Matrix(inputSize, neuronSize);
+        biases = new Matrix(1, neuronSize);
     }
 }
