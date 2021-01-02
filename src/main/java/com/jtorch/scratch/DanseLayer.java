4e006481bd205fdeb842512e6a3d39b74deb5027
Shimon Magal <shimon.magal@gmail.com>
2021-01-02 19:59:25 +0200

    add biases to dense layer

--- src/main/java/com/jtorch/scratch/DanseLayer.java
+++ src/main/java/com/jtorch/scratch/DanseLayer.java
@@ -3,9 +3,15 @@ package com.jtorch.scratch;
 public class DanseLayer {
     private final Matrix weights;
     private final Matrix biases;
+    private Matrix output;
 
     public DanseLayer(int inputSize, int neuronSize) {
         weights = new Matrix(inputSize, neuronSize);
         biases = new Matrix(1, neuronSize);
+    }
+
+    public void forward(Matrix inputs)
+    {
+        output = inputs.multiply(weights).add(biases);
     }
 }
