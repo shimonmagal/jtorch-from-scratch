bae56a674112b3f1e19184a393b97a1619cb3ea2
Shimon Magal <shimon.magal@gmail.com>
2021-01-15 23:35:26 +0200

    mnist testing

--- src/test/java/com/jtorch/scratch/tests/MNISTTests.java
+++ src/test/java/com/jtorch/scratch/tests/MNISTTests.java
@@ -1,2 +1,12 @@
-package com.jtorch.scratch.tests;public class MNISTTests {
+package com.jtorch.scratch.tests;
+
+import com.jtorch.scratch.DanseLayer;
+
+public class MNISTTests {
+//    int epochs = 3;
+//    int batchSize = 30;
+//
+//    for (int i = )
+//
+//    new DanseLayer();
 }
