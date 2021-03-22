d291f34bad8603f1f4bc67d748dd5e9a9f124236
Shimon Magal <shimon.magal@gmail.com>
2021-03-23 01:01:54 +0200

    convert jpeg from MNIST to pixles for testing

--- src/test/java/com/jtorch/scratch/tests/MNISTTests.java
+++ src/test/java/com/jtorch/scratch/tests/MNISTTests.java
@@ -4,15 +4,23 @@ import com.jtorch.scratch.DanseLayer;
 import com.jtorch.scratch.Matrix;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.io.IOException;
 
 public class MNISTTests {
 
     @Test
-    public void testOnePicture()
-    {
-        File f = new File("./resources/MNIST/0/1.png");
+    public void testOnePicture() throws IOException {
+        File f = new File("./src/test/resources/MNIST/0/1.png");
         Matrix input = new Matrix(1, 28*28);
+
+        BufferedImage bufferedImage = ImageIO.read(f);
+        byte[] pixels = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+
+    // we need to convert to grayscale - todo
 
         DanseLayer layer1 = new DanseLayer(28 * 28, 32);
         layer1.forward(input);
